f234e0d82e70e188447ad8ad6e9b750df3cd2702
Canuck1 <jowettpayne@gmail.com>
2023-08-22 16:33:46 -0400

    works for now will center zoom around cursor

--- src/canvas.js
+++ src/canvas.js
@@ -1,13 +1,15 @@
 const { invoke } = window.__TAURI__.tauri
 const canvas = document.getElementById("map-canvas");
 const context = canvas.getContext("2d");
+let sats = [];
 
 
 // Initial zoom and pan values
 let zoom = 1;
 let panX = 0;
 let panY = 0;
-setInterval(drawSats, 1000)
+setInterval(updateSats, 1000);
+setInterval(draw, 1000);
 
 function draw() {
 
@@ -34,32 +36,24 @@ function draw() {
 
     // Draw the image
     context.drawImage(image, drawX, drawY, scaledWidth, scaledHeight);
+    sats.forEach(item => {
+        const marker = new Path2D();
+        marker.moveTo(drawX + zoom * item[0], drawY + zoom * item[1]);
+        marker.lineTo((drawX + zoom * item[0]) - 2 * zoom, (drawY + zoom * item[1]) - 3 * zoom);
+        marker.lineTo((drawX + zoom * item[0]) + 2 * zoom, (drawY + zoom * item[1]) - 3 * zoom);
+        marker.closePath();
+        context.globalAlpha = 0.75;
+        context.fill(marker);
+    })
     
 
     // Restore clipping
     context.restore();
 }
 
-async function drawSats() {
-     // Calculate scaled image dimensions based on zoom
-     const scaledWidth = image.width * zoom;
-     const scaledHeight = image.height * zoom;
- 
-     // Calculate position to draw the image
-     const drawX = canvas.width / 2 - scaledWidth / 2 + panX;
-     const drawY = canvas.height / 2 - scaledHeight / 2 + panY;
-     
+async function updateSats() {
     await invoke("get_all_sat_x_y").then((message) => {
-        draw();
-        message.forEach(item => {
-            console.log("working")
-            const marker = new Path2D();
-            marker.moveTo(drawX + zoom * item[0], drawY + zoom * item[1]);
-            marker.lineTo((drawX + zoom * item[0]) - 2 * zoom, (drawY + zoom * item[1]) - 3 * zoom);
-            marker.lineTo((drawX + zoom * item[0]) + 2 * zoom, (drawY + zoom * item[1]) - 3 * zoom);
-            marker.closePath();
-            context.fill(marker);
-        })
+        sats = message;
     })
 }
 
